e12382e8c491faa1fbd273b16fa8900236068576
harvi <3759harvimendpara@gmail.com>
2024-01-13 20:59:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+var tl = gsap.timeline();
+tl
+    .from("#lowest", {
+        onStart: function () {
+            $('#lowest').textillate({
+                in: {
+                    effect: 'fadeInUp',
+                    callback: function () {
+                        $('#lowest').textillate('out')
+                    }
+                },
+                out: { effect: 'fadeOutUp' }
+            });
+        }
+    })
+
+    .from("#sec-lowest", {
+        opacity: 0,
+        delay: .7,
+        onStart: function () {
+            $('#sec-lowest').textillate({
+                in: {
+                    effect: 'fadeInUp',
+                    callback: function () {
+                        $('#sec-lowest').textillate('out')
+                    }
+                },
+                out: { effect: 'fadeOutUp' }
+            });
+        }
+    })
+
+    .from("#sec-top", {
+        opacity: 0,
+        delay: .7,
+        onStart: function () {
+            $('#sec-top').textillate({
+                in: {
+                    effect: 'fadeInUp',
+                    callback: function () {
+                        $('#sec-top').textillate('out')
+                    }
+                },
+                out: { effect: 'fadeOutUp' }
+            });
+        }
+    })
+
+    .from("#top", {
+        opacity: 0,
+        delay: .7,
+        onStart: function () {
+            $('#top').textillate({
+                in: {
+                    effect: 'fadeInUp'
+                }
+            });
+        }
+    })
+    .to("#top-screen",{
+        top:"-100%",
+        delay:1,
+        duration:1.2,
+        ease:"power4.easeOut"
+    })
